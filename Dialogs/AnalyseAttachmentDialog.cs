7f9ea6b0f63082cb82b05eff3ded3519ec6e16ad
niallermoran <DavinciC0de>
2018-05-31 12:58:43 +0100

    added card with extra detail for image analysis

--- Dialogs/AnalyseAttachmentDialog.cs
+++ Dialogs/AnalyseAttachmentDialog.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.IO;
     using System.Threading.Tasks;
     using System.Web;
@@ -11,6 +12,7 @@
     using System.Net.Http.Headers;
     using Microsoft.ProjectOxford.Face.Contract;
     using Newtonsoft.Json.Linq;
+    using Newtonsoft.Json;
 
     [Serializable]
     internal class AnalyseAttachmentDialog : IDialog<object>
@@ -22,76 +24,178 @@
 
         public async virtual Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-
-            var message = await result;
-            var attachment = message.Attachments[0];
-            using (HttpClient httpClient = new HttpClient())
+            try
             {
-                // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
-                if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase))
-                    && new Uri(attachment.ContentUrl).Host.EndsWith("skype.com"))
-                {
-                    var token = await new MicrosoftAppCredentials().GetTokenAsync();
-                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                }
 
-                var responseMessage = await httpClient.GetAsync(attachment.ContentUrl);
+                // get the attchment
+                var message = await result;
+                var attachment = message.Attachments[0];
 
-                var contentLenghtBytes = responseMessage.Content.Headers.ContentLength;
+                // get the image uploaded
+                var bytes = await GetImage(context, message);
 
-                var bytes = await responseMessage.Content.ReadAsByteArrayAsync();
-
+                // analyse the image using cognitive vision api
                 ImageAnalyzer image = new ImageAnalyzer(bytes);
                 image.ShowDialogOnFaceApiErrors = true;
                 await image.AnalyzeImageAsync(true);
 
+                // detect faces and text from the image
+                await image.DetectFacesAsync(true, true);
+                await image.IdentifyFacesAsync();
+                await image.RecognizeTextAsync();
+
+                // get the results object
+                Models.ImageAnalysisResults results = new Models.ImageAnalysisResults(image);
+
                 if (image.AnalysisResult != null && image.AnalysisResult.Description != null && image.AnalysisResult.Description.Captions.Length > 0)
                 {
-                    var confidence = image.AnalysisResult.Description.Captions[0].Confidence;
-                    var desc = image.AnalysisResult.Description.Captions[0].Text;
-                    if (confidence > 0.95)
-                    {
-                        await context.PostAsync($"I'm a almost certain that this is {desc}");
-                    }
-                    else if (confidence > 0.9)
-                    {
-                        await context.PostAsync($"I'm a pretty sure that this is {desc}");
-                    }
-                    else if (confidence < 0.9 && confidence > 0.5)
-                    {
-                        await context.PostAsync($"I'm a reasonably confident that this is {desc}");
-                    }
-                    else
-                    {
-                        await context.PostAsync($"I'm not sure but at a guess I think this is {desc}");
-                    }
+                    // store this card so we can use the intent dialog to get more information if needed
+                 // context.ConversationData.SetValue<Models.ImageAnalysisResults>("imageanalysed", results);
 
-                    context.ConversationData.SetValue<double>("confidence", confidence);
-
-                    //// see if there were faces
-                    //await image.DetectFacesAsync(true, true);
-                    //await image.IdentifyFacesAsync();
-                    //await image.RecognizeTextAsync();
-
-                    //foreach (Face face in image.DetectedFaces)
-                    //{
-                    //    // Get the border for the associated face id
-                    //    string gender = face.FaceAttributes.Gender;
-                    //    double age = face.FaceAttributes.Age;
-
-                    //    await context.PostAsync($"Face found: " + gender.ToString() + ", age: " + age.ToString());
-                    //}
-
+                    // display the results
+                    var card = context.MakeMessage();
+                    card.Text = results.Description;
+                    await DisplayImageFacts(card, results);
+                    await context.PostAsync(card);              
                 }
                 else
                 {
                     await context.PostAsync($"I'm sorry but I didn't understand the image. It's a bit embarrasing");
                 }
             }
+            catch( Exception ex)
+            {
+                await context.PostAsync($"Oops, somethign went wrong: " + ex.Message);
+            }
 
             context.Wait(this.MessageReceivedAsync);
-
         }
-     
+
+        /// <summary>
+        /// Gets a byte array from an image
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private async Task<byte[]> GetImage(IDialogContext context, IMessageActivity message)
+        {
+            // get the attachment from the message
+            if (message.Attachments.Count > 0)
+            {
+                var attachment = message.Attachments[0];
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
+                    if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase))
+                        && new Uri(attachment.ContentUrl).Host.EndsWith("skype.com"))
+                    {
+                        var token = await new MicrosoftAppCredentials().GetTokenAsync();
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    }
+
+                    var responseMessage = await httpClient.GetAsync(attachment.ContentUrl);
+                    return await responseMessage.Content.ReadAsByteArrayAsync();
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// returns a thumbnail card for displaying the reults of the image analysis
+        /// </summary>
+        /// <returns></returns>
+        private async Task DisplayImageFacts(IMessageActivity message, Models.ImageAnalysisResults image)
+        {
+            var img = image.Image;
+
+            // create a new adaptive card
+            var card = new AdaptiveCards.AdaptiveCard();
+
+            // add a title
+            card.Body.Add( new AdaptiveCards.AdaptiveTextBlock( "Facts about the image:" ));
+
+            // create two columns, tags on left and everything else on right
+            var cs = new AdaptiveCards.AdaptiveColumnSet();
+            cs.Columns.Add(new AdaptiveCards.AdaptiveColumn());
+            cs.Columns.Add(new AdaptiveCards.AdaptiveColumn());
+            cs.Columns[0].Width = "auto";
+            cs.Columns[1].Width = "auto";
+            card.Body.Add( cs );
+
+            #region Tags
+            // create the fact set for the tags
+            var tags = new AdaptiveCards.AdaptiveFactSet();
+
+            // create facts for tags
+            if (img.AnalysisResult.Tags == null || !img.AnalysisResult.Tags.Any() )
+            {
+                tags.Facts.Add(new AdaptiveCards.AdaptiveFact("Tags: ", "no tags"));
+            }
+            else
+            {
+                var list = img.AnalysisResult.Tags.Select(t => new { Confidence = string.Format("{0}%", Math.Round(t.Confidence * 100)), Name = t.Name });
+                foreach( var tag in list)
+                {
+                    tags.Facts.Add(new AdaptiveCards.AdaptiveFact(tag.Name, tag.Confidence.ToString() ));
+                }
+            }
+
+            // add the factset
+            cs.Columns[0].Items.Add(tags);
+            #endregion
+
+            #region Celebrities
+
+            // create a fact set for celebrities and landmarks
+            var celebs = new AdaptiveCards.AdaptiveFactSet();
+          
+            // create facts for celebs
+            if (img.AnalysisResult?.Categories != null)
+            {
+                // just making sure the same celebs and landmarks dont get added twice
+                List<string> celebnames = new List<string>();
+                List<string> landnames = new List<string>();
+
+                foreach (var category in img.AnalysisResult.Categories.Where(c => c.Detail != null))
+                {
+                    dynamic detail = JObject.Parse(category.Detail.ToString());
+                    if (detail.celebrities != null)
+                    {
+                         foreach (var celebrity in detail.celebrities)
+                        {
+                            Models.Celebrity celeb = JsonConvert.DeserializeObject<Models.Celebrity>(celebrity.ToString());
+                            if (!celebnames.Contains(celeb.name))
+                            {
+                                celebnames.Add(celeb.name);
+                                celebs.Facts.Add(new AdaptiveCards.AdaptiveFact(celeb.name, Math.Round(celeb.confidence * 100).ToString() + "%"));
+                            }
+                        }
+                    }
+                    else if (detail.landmarks != null)
+                    {
+                        foreach (var landmark in detail.landmarks)
+                        {
+                            Models.Landmark land = JsonConvert.DeserializeObject<Models.Landmark>(landmark.ToString());
+                            if (!landnames.Contains(land.name))
+                            {
+                                landnames.Add(land.name);
+                                celebs.Facts.Add(new AdaptiveCards.AdaptiveFact(land.name, Math.Round(land.confidence * 100).ToString() + "%"));
+                            }
+                        }
+                    }
+                }
+            }
+
+            cs.Columns[1].Items.Add(celebs);
+
+            #endregion
+
+            // get the attchment
+            Attachment att = new Attachment(contentType: AdaptiveCards.AdaptiveCard.ContentType, content: card);
+
+            // add the attchment to the message
+            message.Attachments.Add(att);
+        }
     }
 }
---
+++ Models/ImageAnalysisResults.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace jfkfiles.bot.Models
+{
+    [Serializable]
+    public class ImageAnalysisResults
+    {
+        public ImageAnalysisResults(ImageAnalyzer img)
+        {
+            this.Image = img;
+
+            if (img.AnalysisResult.Description == null || !img.AnalysisResult.Description.Captions.Any(d => d.Confidence >= 0.2))
+            {
+                this.Description = "Not sure what that is";
+            }
+            else
+            {
+                var confidence = Math.Round( img.AnalysisResult.Description.Captions[0].Confidence *100);
+                var desc = img.AnalysisResult.Description.Captions[0].Text;
+                string title = string.Empty;
+                if (confidence > 95)
+                {
+                    title = $"I'm a almost certain that this is {desc}";
+                }
+                else if (confidence > 90)
+                {
+                    title = $"I'm a pretty sure that this is {desc}";
+                }
+                else if (confidence < 90 && confidence > 50)
+                {
+                    title = $"I'm a reasonably confident that this is {desc}";
+                }
+                else
+                {
+                    title = $"I'm not sure but at a guess I think this is {desc}";
+                }
+                this.Description = title;
+                this.DescriptionConfidence = confidence;
+            }
+
+            var celebNames = this.GetCelebrityNames(img);
+            if (celebNames == null || !celebNames.Any())
+            {
+                this.Celebritynames = string.Empty;
+            }
+            else
+            {
+                this.Celebritynames = string.Join(", ", celebNames.OrderBy(name => name));
+            }
+
+            if (img.AnalysisResult.Color == null)
+            {
+                this.Colours = "Not available" ;
+            }
+            else
+            {
+                var list = new[]
+                {
+                    "Dominant background color:" + img.AnalysisResult.Color.DominantColorBackground,
+                    "Dominant foreground color:" + img.AnalysisResult.Color.DominantColorForeground,
+                    "Dominant colors:" + img.AnalysisResult.Color.DominantColors,
+                    "Accent color:" + "#" + img.AnalysisResult.Color.AccentColor
+                };
+                this.Colours = GetListmarkdown(list);
+            }
+        }
+
+        private IEnumerable<String> GetCelebrityNames(ImageAnalyzer analyzer)
+        {
+            if (analyzer.AnalysisResult?.Categories != null)
+            {
+                foreach (var category in analyzer.AnalysisResult.Categories.Where(c => c.Detail != null))
+                {
+                    dynamic detail = JObject.Parse(category.Detail.ToString());
+                    if (detail.celebrities != null)
+                    {
+                        foreach (var celebrity in detail.celebrities)
+                        {
+
+                            yield return celebrity.name.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private string GetListmarkdown( IEnumerable<string> list )
+        {
+            StringBuilder md = new StringBuilder();
+            int i = 1;
+            foreach( string s in list)
+            {
+                md.AppendFormat("{1}. {0}", s, i.ToString() );
+                i++;
+            }
+            return md.ToString();
+        }
+
+        private string GetMainText(IEnumerable<string> list)
+        {
+            if (list.Count() > 0)
+                return list.First();
+            else
+                return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets a list of tags as markdown for display in a card
+        /// </summary>
+        public string Tags { get; set; }
+
+        public string Colours { get; set; }
+
+        public string Description { get; set; }
+
+        public double DescriptionConfidence { get; set; }
+
+        public string Celebritynames { get; set; }
+
+        public ImageAnalyzer Image { get; set; }
+    }
+
+    public class Celebrity
+    {
+        public faceRectangle faceRectangle { get; set; }
+        public string name { get; set; }
+        public double confidence { get; set; }
+    }
+
+    public class Landmark
+    {
+        public string name { get; set; }
+        public double confidence { get; set; }
+    }
+
+
+    public class faceRectangle
+    {
+        public int top { get; set; }
+        public int left { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+}
